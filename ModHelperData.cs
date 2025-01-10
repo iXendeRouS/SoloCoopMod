b4f269e47993161491d2f9fc4ef51b78ba56e941
ixenderous <ixenderous@gmail.com>
2025-01-10 23:39:28 +1300

    Updated mod description and version

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,11 +3,16 @@ namespace SoloCoopMod;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "44.0";
-    public const string Version = "0.0.1";
+    public const string Version = "0.0.2";
     public const string Name = "Solo Coop";
 
-    public const string Description = 
-        "Play with more T5s and heroes like in Coop mode!" +
+    public const string Description =
+        "Play with more T5s, heroes, and starting cash like in Coop mode! Great for challenges and testing!\n\n" +
+        "Features:\n" +
+        " - Choose how many coop members you want\n" +
+        " - Choose their heroes\n" +
+        " - Choose if you want coop starting cash\n" +
+        " - Choose if coop members have the Double Master Cross Monkey Knowledge\n\n" +
         "Credit to Doombubbles' Unlimited 5th Tiers mod for a starting point.";
 
     public const string RepoOwner = "iXendeRouS"; // TODO add your github username here, also in the download url in README.md
