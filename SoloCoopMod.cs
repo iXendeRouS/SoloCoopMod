ab19f020dca6b9dd54688e71120c876baa92afe2
ixenderous <ixenderous@gmail.com>
2025-03-16 14:04:48 +1300

    code tidy up

--- SoloCoopMod.cs
+++ SoloCoopMod.cs
@@ -1,21 +1,20 @@
 using MelonLoader;
 using BTD_Mod_Helper;
 using BTD_Mod_Helper.Extensions;
-using SoloCoopMod;
+using BTD_Mod_Helper.Api.ModOptions;
+using HarmonyLib;
 using Il2CppAssets.Scripts.Unity.UI_New.InGame;
 using Il2CppAssets.Scripts.Unity;
 using Il2CppAssets.Scripts.Models.SimulationBehaviors;
-using System.Collections.Generic;
 using Il2CppAssets.Scripts.Models;
 using Il2CppAssets.Scripts.Unity.UI_New.InGame.RightMenu;
 using Il2CppAssets.Scripts.Unity.UI_New.InGame.StoreMenu;
-using HarmonyLib;
 using Il2CppAssets.Scripts.Unity.Bridge;
 using Il2CppAssets.Scripts.Models.Towers.Weapons.Behaviors;
 using Il2CppAssets.Scripts.Models.Towers;
 using Newtonsoft.Json.Linq;
-using BTD_Mod_Helper.Api.ModOptions;
-using Il2CppSystem.Runtime.InteropServices;
+using SoloCoopMod;
+using System.Collections.Generic;
 
 [assembly: MelonInfo(typeof(SoloCoopMod.SoloCoopMod), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
@@ -24,51 +23,36 @@ namespace SoloCoopMod;
 
 public class SoloCoopMod : BloonsTD6Mod
 {
-    private static int _activeCoopMembersCount = 0;
-    private static List<string> _selectedHeroIds = new List<string>();
-
-    ModSettingEnum<Heroes>[] heroSettings = [
-        Settings.Hero1,
-        Settings.Hero2,
-        Settings.Hero3,
-        Settings.Hero4
-    ];
+    ModSettingEnum<Heroes>[] heroSettings = [Settings.Hero1, Settings.Hero2, Settings.Hero3, Settings.Hero4];
+    private static List<string> heroes = new List<string>();
 
     public override void OnLoadSettings(JObject settings)
     {
         base.OnLoadSettings(settings);
-        UpdateCachedValues();
+        UpdateHeroes();
     }
 
     public override void OnSaveSettings(JObject settings)
     {
         base.OnSaveSettings(settings);
-        UpdateCachedValues();
+        UpdateHeroes();
     }
 
-    private void UpdateCachedValues()
+    private void UpdateHeroes()
     {
-        _selectedHeroIds = new List<string>();
+        heroes.Clear();
 
         foreach (var heroSetting in heroSettings)
-        {
             if ((Heroes)heroSetting.GetValue() != Heroes.None)
-            {
-                _selectedHeroIds.Add(heroSetting.GetValue().ToString());
-            }
-        }
-
-        _activeCoopMembersCount = _selectedHeroIds.Count;
+                heroes.Add(heroSetting.GetValue().ToString());
     }
 
     public override void OnNewGameModel(GameModel result)
     {
         base.OnNewGameModel(result);
 
-        if (Settings.EnableMod && _activeCoopMembersCount > 0)
-        {
+        if (Settings.EnableMod && heroes.Count > 0)
             ApplyTierRestrictions(result);
-        }
     }
 
     [HarmonyPatch(typeof(UnityToSimulation), nameof(UnityToSimulation.MatchReady))]
@@ -77,7 +61,7 @@ public class SoloCoopMod : BloonsTD6Mod
         [HarmonyPostfix]
         internal static void Postfix()
         {
-            if (Settings.EnableMod && _activeCoopMembersCount > 0)
+            if (Settings.EnableMod && heroes.Count > 0)
             {
                 ApplyCoopHeroes();
                 ApplyCoopStartingCash();
@@ -85,33 +69,22 @@ public class SoloCoopMod : BloonsTD6Mod
         }
     }
 
-    // Allow multiple T5s with special handling for Master Double Master Cross MK setting
     private static void ApplyTierRestrictions(GameModel model)
     {
         var restrictionList = new List<TowerTierRestrictionModel>(model.towerTierRestrictions);
 
         foreach (var item in GameModelExt.GetAllShopTowerDetails(model))
-        {
             for (var i = 0; i < 3; i++)
-            {
-                restrictionList.Add(new TowerTierRestrictionModel(item.towerId + "Restriction", item.towerId, i, 5, _activeCoopMembersCount - 1));
-            }
-        }
+                restrictionList.Add(new TowerTierRestrictionModel(item.towerId + "Restriction", item.towerId, i, 5, heroes.Count - 1));
 
         if (Settings.MasterDoubleCross)
-        {
-            restrictionList.Add(new TowerTierRestrictionModel("DartMonkey" + "Restriction", "DartMonkey", 2, 5, _activeCoopMembersCount - 1));
-        }
+            restrictionList.Add(new TowerTierRestrictionModel("DartMonkey" + "Restriction", "DartMonkey", 2, 5, heroes.Count - 1));
 
         model.towerTierRestrictions = restrictionList.ToArray();
 
         foreach (TowerModel towerModel in model.towers)
-        {
             if (towerModel.tier >= 5)
-            {
                 towerModel.GetDescendants<LimitProjectileModel>().ForEach(lpmodel => lpmodel.globalForPlayer = false);
-            }
-        }
     }
 
     private static void ApplyCoopHeroes()
@@ -119,16 +92,11 @@ public class SoloCoopMod : BloonsTD6Mod
         var towerMaxes = InGame.instance.GetTowerInventory().towerMaxes;
 
         foreach (var hero in Game.instance.GetHeroDetailModels())
-        {
             towerMaxes[hero.towerId] = 0;
-        }
 
-        foreach (var heroId in _selectedHeroIds)
-        {
-            if (Game.instance.GetHeroDetailModels().Any(h => h.towerId == heroId)) {
+        foreach (var heroId in heroes)
+            if (Game.instance.GetHeroDetailModels().Any(h => h.towerId == heroId))
                 towerMaxes[heroId]++;
-            }
-        }
 
         RefreshShop();
     }
@@ -136,15 +104,14 @@ public class SoloCoopMod : BloonsTD6Mod
     private static void RefreshShop()
     {
         ShopMenu.instance.RebuildTowerSet();
+
         foreach (var button in ShopMenu.instance.ActiveTowerButtons)
-        {
             button.Cast<TowerPurchaseButton>().Update();
-        }
     }
 
     private static void ApplyCoopStartingCash()
     {
-        if (!Settings.UseCoopStartingCash || InGameData.CurrentGame.IsSandbox || _activeCoopMembersCount < 2 || InGame.instance.GetSimulation().roundTime.elapsed != 0) return;
-        InGame.instance.AddCash((_activeCoopMembersCount - 1) * 650 - _activeCoopMembersCount * _activeCoopMembersCount * 50);
+        if (!Settings.UseCoopStartingCash || InGame.instance.GetSimulation().roundTime.elapsed != 0 || InGameData.CurrentGame.IsSandbox || heroes.Count < 2) return;
+        InGame.instance.AddCash((heroes.Count - 1) * 650 - heroes.Count * heroes.Count * 50);
     }
 }
